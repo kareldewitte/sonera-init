7027e442de791099e53b73b28113439bc4301e7b
Karel De Witte <karel.dewitte@magnolia-cms.com>
2015-06-17 14:05:22 +0300

    init

---
+++ products-module/src/main/java/fi/sonera/products/ProductsModule.java
@@ -0,0 +1,12 @@
+package fi.sonera.products;
+
+/**
+ * This class is optional and represents the configuration for the products-module module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/products-module</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class ProductsModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
